1bd57c633c6e55616afd8ec17a213d1ab727ecfe
Nairyn <nairyn@yandex.ru>
2025-08-11 20:55:30 +0300

    AddedPauseMenuAndButtonBack
    Добавил простенькую меню паузы и кнопку назад, которая активна, если меню включено из паузы и неактивна, если игрок гибнет

--- Assets/Scripts/UI/FinalMenu.cs
+++ Assets/Scripts/UI/FinalMenu.cs
@@ -2,15 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FinalMenu : MonoBehaviour
 {
     [SerializeField] private GameObject _finalPanel;
+    [SerializeField] private Button _backBatton;
     [SerializeField] private TMP_Text _score;
 
-    public void FinalPanelActive()
+    public void FinalPanelActive(bool isDead)
     {
-        StartCoroutine(DelayBeforeFinalPanelActive());
+        _finalPanel.SetActive(true);
+        _backBatton.interactable = isDead;       
+        _score.text = "Scotre: " + ScoreCount.Instance.Score;
+        Time.timeScale = 0.0f;
+    }
+
+    public void SetPanelAfterDeath(bool isDead)
+    {
+        StartCoroutine(DelayBeforeFinalPanelActive(isDead));
     }
 
     public void TimeScaleRestar()
@@ -18,11 +28,9 @@ public class FinalMenu : MonoBehaviour
         Time.timeScale = 1.0f;
     }
 
-    IEnumerator DelayBeforeFinalPanelActive()
+    IEnumerator DelayBeforeFinalPanelActive(bool isDead)
     {
         yield return new WaitForSeconds(1.3f);
-        _finalPanel.SetActive(true);
-        _score.text = "Scotre: " + ScoreCount.Instance.Score;
-        Time.timeScale = 0.0f;
+        FinalPanelActive(isDead);
     }
 }
